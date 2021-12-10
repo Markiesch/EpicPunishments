6df4f73c78f8556e73701100f44768f12ae0d55a
Markiesch <marksmeel@gmail.com>
2021-12-10 22:39:23 +0100

    Removed unused import

--- src/com/markiesch/commands/UnmuteCommand.java
+++ src/com/markiesch/commands/UnmuteCommand.java
@@ -4,7 +4,6 @@ import com.markiesch.EpicPunishments;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
