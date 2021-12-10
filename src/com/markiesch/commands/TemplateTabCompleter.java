071780dec1503b9b2e30b6775a47690400131505
Markiesch <marksmeel@gmail.com>
2021-12-10 22:39:33 +0100

    Removed unused import

--- src/com/markiesch/commands/TemplateTabCompleter.java
+++ src/com/markiesch/commands/TemplateTabCompleter.java
@@ -7,7 +7,6 @@ import org.bukkit.configuration.ConfigurationSection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
 
 public class TemplateTabCompleter {
     static EpicPunishments plugin = EpicPunishments.getInstance();
