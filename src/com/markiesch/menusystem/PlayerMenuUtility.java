041d852d500ef8dca358f97e35686c590448f024
Markiesch <marksmeel@gmail.com>
2021-12-12 16:37:28 +0100

    Added reset funtion to playermenuUtils

--- src/com/markiesch/menusystem/PlayerMenuUtility.java
+++ src/com/markiesch/menusystem/PlayerMenuUtility.java
@@ -8,17 +8,28 @@ public class PlayerMenuUtility {
     private final Player owner;
     private String reason;
     private String templateName;
+    private Long duration;
     private UUID uuid;
 
     public PlayerMenuUtility(Player owner) { this.owner = owner; }
     public Player getOwner() { return owner; }
 
-    public String getReason() { return reason; }
-    public void setReason(String reason) { this.reason = reason; }
-
     public String getTemplateName() { return templateName; }
     public void setTemplateName(String templateName) { this.templateName = templateName; }
 
+    public String getReason() { return reason; }
+    public void setReason(String reason) { this.reason = reason; }
+
+    public Long getDuration() { return duration; }
+    public void setDuration(Long duration) { this.duration = duration; }
+
     public UUID getUUID() { return uuid; }
     public void setUUID(UUID uuid) { this.uuid = uuid; }
+
+    public void reset() {
+        this.reason = null;
+        this.templateName = null;
+        this.duration = null;
+        this.uuid = null;
+    }
 }
