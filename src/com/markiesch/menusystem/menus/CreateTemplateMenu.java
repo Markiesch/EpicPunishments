49e8dbce4b576d261e0878fb8147edc7bc3b4766
Markiesch <marksmeel@gmail.com>
2021-12-19 15:38:34 +0100

    Added slot variables

--- src/com/markiesch/menusystem/menus/CreateTemplateMenu.java
+++ src/com/markiesch/menusystem/menus/CreateTemplateMenu.java
@@ -15,13 +15,17 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.UUID;
-
 public class CreateTemplateMenu extends Menu implements Listener {
     EpicPunishments plugin = EpicPunishments.getInstance();
     private final String name;
     private final String reason;
     private String type = "KICK";
+
+    private final int NAME_SLOT = 13;
+    private final int TYPE_SLOT = 19;
+    private final int DURATION_SLOT = 22;
+    private final int REASON_SLOT = 25;
+    private final int CREATE_SLOT = 40;
 
     public String getMenuName() { return "Templates > Create Template"; }
     public int getSlots() { return 54; }
@@ -35,15 +39,18 @@ public class CreateTemplateMenu extends Menu implements Listener {
     public void handleMenu(InventoryClickEvent event) {
         if (event.getCurrentItem() == null) return;
         Player player = (Player) event.getWhoClicked();
-        UUID uuid = player.getUniqueId();
-        Material clickedItem = event.getCurrentItem().getType();
 
-        if (clickedItem.equals(Material.WRITABLE_BOOK)) {
-            plugin.getEditor().put(uuid, new InputUtils(InputTypes.CREATE_TEMPLATE_REASON, player, "§bTemplate Reason", "§7Type in a template reason"));
+        if (event.getSlot() == NAME_SLOT) {
+            plugin.getEditor().put(player.getUniqueId(), new InputUtils(InputTypes.CREATE_TEMPLATE_NAME, player, "§bTemplate Name", "§7Type in a new name"));
             return;
         }
 
-        if (clickedItem.equals(Material.EMERALD_BLOCK)) {
+        if (event.getSlot() == REASON_SLOT) {
+            plugin.getEditor().put(player.getUniqueId(), new InputUtils(InputTypes.CREATE_TEMPLATE_REASON, player, "§bTemplate Reason", "§7Type in a template reason"));
+            return;
+        }
+
+        if (event.getSlot() == CREATE_SLOT) {
             PlayerMenuUtility playerMenuUtility = EpicPunishments.getPlayerMenuUtility(player);
             String name = playerMenuUtility.getTemplateName();
             String reason = playerMenuUtility.getReason();
@@ -54,7 +61,7 @@ public class CreateTemplateMenu extends Menu implements Listener {
             return;
         }
 
-        if (event.getSlot() == 19) {
+        if (event.getSlot() == TYPE_SLOT) {
             ItemStack item = event.getCurrentItem();
             ItemMeta meta = item.getItemMeta();
             if (meta == null) return;
@@ -72,16 +79,18 @@ public class CreateTemplateMenu extends Menu implements Listener {
         if (reason == null) reason = "None";
 
         ItemStack template = ItemUtils.createItem(Material.PAPER, "§c§l" + name, "", "§cType: §7" + type, "§cReason: §7" + reason);
-        inventory.setItem(13, template);
+        inventory.setItem(NAME_SLOT, template);
 
         ItemStack typeItem = ItemUtils.createItem(getType(), "§c§l" + type, "§7Click to toggle type");
-        inventory.setItem(19, typeItem);
+        inventory.setItem(TYPE_SLOT, typeItem);
+
+        // TODO duration item
 
         ItemStack reasonItem = ItemUtils.createItem(Material.WRITABLE_BOOK, "§c§lReason", "§7Click to insert reason", "", "§7Reason set: §c" + reason);
-        inventory.setItem(25, reasonItem);
+        inventory.setItem(REASON_SLOT, reasonItem);
 
         ItemStack createItem = ItemUtils.createItem(Material.EMERALD_BLOCK, "§c§lCreate Template", "§7Click to confirm settings", "", "§7Reason set: §c" + reason);
-        inventory.setItem(40, createItem);
+        inventory.setItem(CREATE_SLOT, createItem);
     }
 
     public Material getType() {
