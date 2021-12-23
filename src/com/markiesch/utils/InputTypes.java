20737dffa2a7ddd12df60298c8cddb1d45bff611
Markiesch <marksmeel@gmail.com>
2021-12-23 21:48:18 +0100

    Moved file to utils

--- src/com/markiesch/utils/InputTypes.java
+++ src/com/markiesch/utils/InputTypes.java
@@ -1,4 +1,4 @@
-package com.markiesch.menusystem;
+package com.markiesch.utils;
 
 public enum InputTypes {
     CREATE_TEMPLATE_NAME, CREATE_TEMPLATE_REASON, CREATE_TEMPLATE_DURATION, EDIT_TEMPLATE_NAME, EDIT_TEMPLATE_REASON, EDIT_TEMPLATE_DURATION
