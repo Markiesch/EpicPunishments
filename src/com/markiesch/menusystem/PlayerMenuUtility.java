3900421b28bf94845fc7864406e20473189a75b5
Markiesch <marksmeel@gmail.com>
2021-12-23 21:48:59 +0100

    Added type getters and setters, added fillEmptyField method

--- src/com/markiesch/menusystem/PlayerMenuUtility.java
+++ src/com/markiesch/menusystem/PlayerMenuUtility.java
@@ -6,19 +6,23 @@ import java.util.UUID;
 
 public class PlayerMenuUtility {
     private final Player owner;
+    private String name;
     private String reason;
-    private String templateName;
+    private String type;
     private Long duration;
     private UUID uuid;
 
     public PlayerMenuUtility(Player owner) { this.owner = owner; }
     public Player getOwner() { return owner; }
 
-    public String getTemplateName() { return templateName; }
-    public void setTemplateName(String templateName) { this.templateName = templateName; }
+    public String getTemplateName() { return name; }
+    public void setTemplateName(String templateName) { this.name = templateName; }
 
     public String getReason() { return reason; }
     public void setReason(String reason) { this.reason = reason; }
+
+    public String getType() { return type; }
+    public void setType(String reason) { this.type = reason; }
 
     public Long getDuration() { return duration; }
     public void setDuration(Long duration) { this.duration = duration; }
@@ -27,9 +31,17 @@ public class PlayerMenuUtility {
     public void setUUID(UUID uuid) { this.uuid = uuid; }
 
     public void reset() {
-        this.reason = null;
-        this.templateName = null;
-        this.duration = null;
-        this.uuid = null;
+        reason = null;
+        name = null;
+        type = null;
+        duration = null;
+        uuid = null;
+    }
+
+    public void fillEmptyFields() {
+        name = name == null ? "new_template" : name;
+        reason = reason == null ? "none" : reason;
+        type = type == null ? "KICK" : type;
+        duration = duration == null ? 0L : duration;
     }
 }
