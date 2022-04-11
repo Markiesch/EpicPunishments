d812e3432d47fb7d2534cc5f609d65aa5fe6991b
Markiesch <marksmeel@gmail.com>
2022-04-11 22:11:10 +0200

    Fixed incorrect Query syntax

--- src/main/java/com/markiesch/storage/SQL/Query.java
+++ src/main/java/com/markiesch/storage/SQL/Query.java
@@ -3,33 +3,35 @@ package com.markiesch.storage.SQL;
 public enum Query {
     CREATE_INFRACTION_TABLE(
             "CREATE TABLE IF NOT EXISTS Infraction (" +
-                "[id]         INT                 PRIMARY KEY        AUTOINCREMENT," +
+                "[id]         INTEGER                 PRIMARY KEY        AUTOINCREMENT," +
                 "[victim]     VARCHAR(36)         NOT NULL," +
                 "[issuer]     VARCHAR(36)," +
                 "[type]       VARCHAR(10)," +
                 "[reason]     VARCHAR(100)," +
-                "[duration]   INT," +
+                "[duration]   INTEGER," +
                 "[date]       DATE                NOT NULL" +
             ");"
     ),
     CREATE_PLAYER_TABLE(
-            "CREATE TABLE IF NOT EXISTS Player (" +
+            "CREATE TABLE IF NOT EXISTS Profile (" +
                 "[UUID]       VARCHAR(36)     PRIMARY KEY," +
                 "[ipHistory]  LONGTEXT" +
             ");"
     ),
     CREATE_TEMPLATE_TABLE(
             "CREATE TABLE IF NOT EXISTS Template (" +
-                "[id]           INT                 PRIMARY KEY     AUTOINCREMENT," +
+                "[id]           INTEGER                 PRIMARY KEY     AUTOINCREMENT," +
                 "[name]         VARCHAR(50)         NOT NULL," +
                 "[type]         VARCHAR(10)         NOT NULL," +
                 "[reason]       VARCHAR(100)," +
-                "[duration]     INT" +
+                "[duration]     INTEGER" +
             ");"
     ),
-    ADD_PROFILE("INSERT INTO Player (UUID) VALUES(?)"),
-    SELECT_PROFILE("SELECT * FROM Player WHERE UUID = ?"),
-    SELECT_PROFILES("SELECT * FROM Player");
+    ADD_PROFILE(
+            "INSERT INTO Profile (UUID) VALUES(?) "
+    ),
+    SELECT_PROFILE("SELECT * FROM Profile WHERE UUID = ?"),
+    SELECT_PROFILES("SELECT * FROM Profile");
 
     private final String query;
 
