d23a696a2b48d067b7d65c47a7c66817f5e9d8cb
Markiesch <marksmeel@gmail.com>
2022-04-11 22:11:58 +0200

    Removed unused code

--- src/main/java/com/markiesch/controllers/ProfileController.java
+++ src/main/java/com/markiesch/controllers/ProfileController.java
@@ -3,16 +3,13 @@ package com.markiesch.controllers;
 import com.markiesch.EpicPunishments;
 import com.markiesch.models.ProfileModel;
 
-import java.util.HashMap;
 import java.util.UUID;
 
 public class ProfileController {
     private final EpicPunishments plugin;
-    private final HashMap<UUID, ProfileModel> profiles;
 
     public ProfileController(EpicPunishments plugin) {
         this.plugin = plugin;
-        profiles = new HashMap<>();
     }
 
     /**
@@ -21,24 +18,9 @@ public class ProfileController {
      * @return the newly created profile
      */
     public ProfileModel createProfile(UUID uuid) {
-        if (isPlayerRegistered(uuid)) return getPlayer(uuid);
-
         ProfileModel profile = new ProfileModel(plugin, uuid);
         plugin.getStorage().createProfile(profile);
-        profiles.put(uuid, profile);
 
         return profile;
-    }
-
-    public ProfileModel getProfile(UUID uuid) {
-        return profiles.get(uuid);
-    }
-
-    public boolean isPlayerRegistered(UUID uuid) {
-        return profiles.containsKey(uuid);
-    }
-
-    public ProfileModel getPlayer(UUID uuid) {
-        return profiles.get(uuid);
     }
 }
