3587e995dfce7d7317433d9e5faf9d411ab3ad89
Markiesch <marksmeel@gmail.com>
2022-04-13 22:18:58 +0200

    Added menusystem

--- src/main/java/com/markiesch/listeners/PlayerJoin.java
+++ src/main/java/com/markiesch/listeners/PlayerJoin.java
@@ -6,13 +6,16 @@ import com.markiesch.models.ProfileModel;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.UUID;
 
 public class PlayerJoin implements Listener {
+    private final EpicPunishments plugin;
     private final ProfileController profileController;
 
     public PlayerJoin(EpicPunishments plugin) {
+        this.plugin = plugin;
         this.profileController = plugin.getProfileController();
     }
 
@@ -20,17 +23,23 @@ public class PlayerJoin implements Listener {
     public void onPlayerPreLogin(AsyncPlayerPreLoginEvent event) {
         UUID uuid = event.getUniqueId();
 
-        profileController.createProfile(uuid);
+        profileController.createProfile(uuid, event.getAddress().getHostName());
 
         ProfileModel profile = profileController.getProfile(uuid);
 
         if (profile == null) {
-            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, "You profile couldn't be created or found, please relog");
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, "You profile couldn't be created or found, please rejoin");
             return;
         }
 
         if (profile.isBanned()) {
             event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_BANNED, "The ban hammer has spoken...");
         }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        
+        plugin.playerMenuUtilityMap.remove(event.getPlayer().getUniqueId());
     }
 }
