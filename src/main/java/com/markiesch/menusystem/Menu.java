fd794208770a821a8fdb06054c6bb13e885ef4b8
Markiesch <marksmeel@gmail.com>
2022-11-09 00:24:23 +0100

    Added permission check

--- src/main/java/com/markiesch/menusystem/Menu.java
+++ src/main/java/com/markiesch/menusystem/Menu.java
@@ -1,5 +1,6 @@
 package com.markiesch.menusystem;
 
+import com.markiesch.EpicPunishments;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -8,22 +9,30 @@ import org.bukkit.inventory.InventoryHolder;
 
 public abstract class Menu implements InventoryHolder {
     protected PlayerMenuUtility playerMenuUtility;
-    private final String menuName;
+    protected final EpicPunishments plugin;
     private final int slots;
     private Inventory inventory;
 
-    public Menu(PlayerMenuUtility playerMenuUtility, String menuName, int slots) {
+    public Menu(EpicPunishments plugin, PlayerMenuUtility playerMenuUtility, int slots) {
+        this.plugin = plugin;
         this.playerMenuUtility = playerMenuUtility;
-        this.menuName = menuName;
         this.slots = slots;
     }
 
+    public abstract String getMenuName();
+    public abstract String getRequiredPermission();
     public abstract void handleMenu(InventoryClickEvent event);
     public abstract void setMenuItems();
 
     public void open() {
         Player player = playerMenuUtility.getOwner();
-        inventory = Bukkit.createInventory(this, slots, menuName);
+
+        if (getRequiredPermission() != null && !player.hasPermission(getRequiredPermission())) {
+            player.sendMessage("§7You do not have§c permissions§7 to view templates");
+            player.closeInventory();
+            return;
+        }
+        inventory = Bukkit.createInventory(this, slots, this.getMenuName());
         this.setMenuItems();
         player.openInventory(inventory);
     }
