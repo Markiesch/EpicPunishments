db9eba782bc0c4e3d6ccaf7a6925055ba42e763f
Markiesch <marksmeel@gmail.com>
2022-11-09 00:25:06 +0100

    Removed unused code

--- src/main/java/com/markiesch/utils/ItemUtils.java
+++ src/main/java/com/markiesch/utils/ItemUtils.java
@@ -31,36 +31,4 @@ public class ItemUtils {
 
         return item;
     }
-
-//    public static ItemStack createPlayerHead(PlayerModel playerModel) {
-//        EpicPunishments plugin = EpicPunishments.getInstance();
-//
-//        List<InfractionModel> infractionsList = playerModel.getInfractions();
-//        OfflinePlayer player = playerModel.getPlayer();
-//
-//        long date = player.getFirstPlayed();
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
-//        String formattedDate = sdf.format(date);
-//
-//        ItemStack playerHead = createItem(
-//                Material.PLAYER_HEAD,
-//                "§9§l" + player.getName(),
-//                "§bLeft Click §7to manage player",
-//                "§bRight Click §7to teleport",
-//                "",
-//                (infractionsList.isEmpty() ? "§a✔ §7didn't received any punishments yet" : "§6✔ §7had received " + infractionsList.size() + " punishments"),
-//                (playerModel.isBanned() ? "§6✔ §7" + player.getName() + " is§a banned §7on §e" + plugin.getServer().getName() : "§a✔ §a" + player.getName() + " §7is not§e banned"),
-//                "",
-//                "§7Joined at: " + formattedDate
-//        );
-//
-//        SkullMeta playerMeta = (SkullMeta) playerHead.getItemMeta();
-//        if (playerMeta != null) {
-//            playerMeta.setOwningPlayer(player);
-//            playerMeta.getPersistentDataContainer().set(new NamespacedKey(plugin, "uuid"), PersistentDataType.STRING, player.getUniqueId().toString());
-//            playerHead.setItemMeta(playerMeta);
-//        }
-//
-//        return playerHead;
-//    }
 }
