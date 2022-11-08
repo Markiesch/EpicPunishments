bb61a972d4baf93ad6c75a6f36c061d391d6cb3b
Markiesch <marksmeel@gmail.com>
2022-11-09 00:22:45 +0100

    Created kick command

---
+++ src/main/java/com/markiesch/commands/KickCommand.java
@@ -0,0 +1,48 @@
+package com.markiesch.commands;
+
+import com.markiesch.modules.infraction.PreparedInfraction;
+import com.markiesch.utils.InfractionType;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KickCommand extends CommandBase {
+    public KickCommand() {
+        super(
+                "kick",
+                "epicpunishments.kick",
+                "§7Usage: §e/kick <target> (reason)",
+                1,
+                -1,
+                true
+        );
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, String[] args) {
+        Player issuer = (Player) sender;
+
+        OfflinePlayer target = Bukkit.getPlayer(args[0]);
+        if (target == null) target = Bukkit.getOfflinePlayer(args[0]);
+
+        List<String> arguments = Arrays.asList(args);
+        String reason = String.join(" ", arguments.subList(1, arguments.size()));
+
+        new PreparedInfraction(
+                InfractionType.KICK,
+                reason
+        ).execute(issuer, target);
+
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, String alias, String[] args) {
+        return new ArrayList<>();
+    }
+}
