110592fa06eca37669a074cfdde09fa8bb2b8032
Markiesch <marksmeel@gmail.com>
2022-11-12 20:48:02 +0100

    Added createPlayerHead util

--- src/main/java/com/markiesch/utils/ItemUtils.java
+++ src/main/java/com/markiesch/utils/ItemUtils.java
@@ -1,13 +1,18 @@
 package com.markiesch.utils;
 
+import com.markiesch.modules.infraction.InfractionModel;
+import com.markiesch.modules.profile.ProfileController;
+import com.markiesch.modules.profile.ProfileModel;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class ItemUtils {
     public static ItemStack createItem(Material material, String name, String... lore) {
@@ -30,5 +35,30 @@ public class ItemUtils {
         }
 
         return item;
+    }
+
+    public static ItemStack createPlayerInfoHead(UUID uuid) {
+        OfflinePlayer player = Bukkit.getOfflinePlayer(uuid);
+
+        Date date = new Date(player.getFirstPlayed());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+        String formattedDate = sdf.format(date);
+
+        ProfileModel profile = new ProfileController().getProfile(uuid);
+        List<InfractionModel> infractions = profile.getInfractions();
+
+        ItemStack playerHead = ItemUtils.createItem(
+                Material.PLAYER_HEAD,
+                "§b§l" + player.getName(),
+                (infractions.isEmpty() ? "§a✔ §7didn't received any punishments yet" : "§6✔ §7had received " + infractions.size() + " punishments"),
+                (profile.isBanned() ? "§6✔ §7" + player.getName() + " is§a banned" : "§a✔ §a" + player.getName() + " §7is not§e banned"),
+                "",
+                "§7Joined at: " + formattedDate);
+
+        SkullMeta playerMeta = (SkullMeta) playerHead.getItemMeta();
+        if (playerMeta != null) playerMeta.setOwningPlayer(player);
+        playerHead.setItemMeta(playerMeta);
+
+        return playerHead;
     }
 }
