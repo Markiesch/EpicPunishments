ca982b75b224a6bf5163d8925db90f1953859f86
Markiesch <marksmeel@gmail.com>
2022-12-20 21:18:01 +0100

    Removed teleport button

--- src/main/java/com/markiesch/menusystem/menus/PlayerMenu.java
+++ src/main/java/com/markiesch/menusystem/menus/PlayerMenu.java
@@ -13,9 +13,8 @@ import org.bukkit.inventory.ItemStack;
 import java.util.UUID;
 
 public class PlayerMenu extends Menu implements Listener {
-    private static final byte TELEPORT_BUTTON_SLOT = 20;
-    private static final byte PUNISH_BUTTON_SLOT = 22;
-    private static final byte INFRACTIONS_BUTTON_SLOT = 24;
+    private static final byte PUNISH_BUTTON_SLOT = 21;
+    private static final byte INFRACTIONS_BUTTON_SLOT = 23;
     private static final byte BACK_BUTTON_SLOT = 40;
 
     public OfflinePlayer target;
@@ -43,15 +42,6 @@ public class PlayerMenu extends Menu implements Listener {
             getOwner().closeInventory();
             return;
         }
-
-        ItemStack teleportButton = ItemUtils.createItem(
-                Material.ENDER_EYE,
-                Translation.MENU_PLAYER_TELEPORT_TITLE.toString(),
-                Translation.MENU_PLAYER_TELEPORT_LORE.addPlaceholder("name", target.getName()).toList()
-        );
-        setButton(TELEPORT_BUTTON_SLOT, teleportButton, event -> {
-            // TODO teleport to player
-        });
 
         ItemStack punishButton = ItemUtils.createItem(
                 Material.ANVIL,
