7f52c7cbf89baa3f3b5b0e161e9f954e6eaa282d
Markiesch <marksmeel@gmail.com>
2023-01-07 17:03:15 +0100

    Feat: added clearchat to help message

--- src/main/java/com/markiesch/commands/PunishCommand.java
+++ src/main/java/com/markiesch/commands/PunishCommand.java
@@ -57,6 +57,7 @@ public class PunishCommand extends CommandBase {
                 "&c&lEpicPunishments &7v" + plugin.getDescription().getVersion(),
                 " ",
                 "&9/punish (help | reload) &7opens the main menu",
+                "&9/clearchat &7clears the chat for all online players",
                 "&9/ban &e<player> <duration> (reason) &7bans the mentioned player",
                 "&9/unban &e<player> &7will unban the mentioned player",
                 "&9/mute &e<player> <duration> (reason) &7mutes the mentioned player",
