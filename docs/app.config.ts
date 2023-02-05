2e68fd1da2c65f8675937807b89e58c212c2e839
Markiesch <marksmeel@gmail.com>
2023-02-05 23:15:59 +0100

    Docs: initial commit

---
+++ docs/app.config.ts
@@ -0,0 +1,24 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Epic Punishments',
+    description: 'The best place to start your documentation.',
+    socials: {
+      github: 'Markiesch/EpicPunishments'
+    },
+    aside: {
+      level: 1,
+    },
+    main: {
+      fluid: true,
+      padded: true
+    },
+    header: {
+      logo: true,
+      fluid: true,
+    },
+    footer: {
+      credits: false,
+      fluid: true
+    }
+  }
+})
